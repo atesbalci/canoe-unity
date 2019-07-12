687916ad72ac22712af42c5e35be6f81c537e04a
Ates Balci <abalci@zynga.com>
2019-07-12 18:32:03 +0300

    Added noisy waves

---
+++ Assets/Canoe/Scripts/Helpers/Waves/WaveBehaviour.cs
@@ -0,0 +1,143 @@
+﻿using System.Linq;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Canoe.Helpers.Waves
+{
+    public class WaveBehaviour : MonoBehaviour
+    {
+        [Header("Sea Parameters")]
+        public int XDimension;
+        public int YDimension;
+        public float Gap;
+
+        [Header("Wave Parameters")]
+        public float Speed;
+        public float Period;
+        public float WaveSize;
+        public float WrinkleFrequency;
+        public float WrinkleSize;
+        public float NoiseSize;
+        public float NoiseScale;
+
+        private Vector3[] _vertices;
+        
+        // Mesh instance
+        private Mesh _seaMesh;
+
+        private void Start()
+        {
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            _seaMesh = GenerateMesh(XDimension, YDimension, Gap);
+            GetComponent<MeshFilter>().sharedMesh = _seaMesh;
+            _vertices = _seaMesh.vertices.ToArray();
+        }
+
+        private static Mesh GenerateMesh(int dimX, int dimY, float gap)
+        {
+            var mesh =  new Mesh();
+            var vertices = new Vector3[dimX * dimY * 6];
+            var triangles = new int[vertices.Length];
+            var ind = 0;
+            for (var y = 0; y < dimY; y++)
+            {
+                for (var x = 0; x < dimX; x++)
+                {
+                    var pos = new Vector3(x - dimX / 2f, 0, y - dimY / 2f);
+
+                    //Triangle 1
+                    vertices[ind] = pos * gap;
+                    vertices[ind + 1] = (pos + Vector3.forward) * gap;
+                    vertices[ind + 2] = (pos + Vector3.right) * gap;
+                    triangles[ind] = ind;
+                    triangles[ind + 1] = ind + 1;
+                    triangles[ind + 2] = ind + 2;
+                    ind += 3;
+
+                    //Triangle 2
+                    vertices[ind] = (pos + Vector3.forward) * gap;
+                    vertices[ind + 1] = (pos + Vector3.right + Vector3.forward) * gap;
+                    vertices[ind + 2] = (pos + Vector3.right) * gap;
+                    triangles[ind] = ind;
+                    triangles[ind + 1] = ind + 1;
+                    triangles[ind + 2] = ind + 2;
+                    ind += 3;
+                }
+            }
+            mesh.vertices = vertices;
+            mesh.triangles = triangles;
+            mesh.RecalculateNormals();
+            return mesh;
+        }
+        
+        private struct WaveJob : IJobParallelFor
+        {
+            public float Gap;
+
+            public float Speed;
+            public float Period;
+            public float WaveSize;
+            public float WrinkleFrequency;
+            public float WrinkleSize;
+            public float NoiseSize;
+            public float NoiseScale;
+
+            public float2 Size;
+            public float Time;
+
+            public NativeArray<Vector3> Vertices;
+            public NativeArray<Vector3> VerticesCur;
+            
+            public void Execute(int i)
+            {
+                var loc = (((float3) Vertices[i]).xz / Size + new float2(0f, Time)) * NoiseSize;
+                var n = noise.cnoise(loc) * NoiseScale;
+                float3 cur = Vertices[i];
+                float3 vert = cur;
+                cur.y = (math.sin(vert.z * Period + Time * Speed) + math.sin(vert.z * Period * 0.4f + Time * Speed) + n) * WaveSize;
+                cur.z = vert.z + math.sin((vert.x + Time * Gap) * WrinkleFrequency) * WrinkleSize + math.sin((vert.z * math.sin(Time) + vert.x * math.cos(Time)) * Period * 0.4f + Time * Speed);
+                VerticesCur[i] = cur;
+            }
+        }
+
+        private void Update()
+        {
+            float time = Time.time;
+            var size = new float2(XDimension, YDimension);
+            
+            var vertices = new NativeArray<Vector3>(_vertices, Allocator.Persistent);
+            var verticesCur = new NativeArray<Vector3>(_vertices.Length, Allocator.Persistent);
+            
+            var job = new WaveJob
+            {
+                Size = size,
+                Time = time,
+                Vertices = vertices,
+                NoiseScale = NoiseScale,
+                NoiseSize = NoiseSize,
+                VerticesCur = verticesCur,
+                Gap = Gap,
+                Period = Period,
+                Speed = Speed,
+                WaveSize = WaveSize,
+                WrinkleFrequency = WrinkleFrequency,
+                WrinkleSize = WrinkleSize
+            };
+
+            job.Schedule(_vertices.Length, 1).Complete();
+
+            _seaMesh.vertices = job.VerticesCur.ToArray();
+            
+            vertices.Dispose();
+            verticesCur.Dispose();
+            // Makes the lighting better :D
+            _seaMesh.RecalculateNormals();
+        }
+    }
+}
--- Assets/Canoe/Shaders/Sea.shader
+++ Assets/Canoe/Shaders/Sea.shader
@@ -1,79 +1,27 @@
 Shader "Canoe/Sea" {
 Properties {
     _MainColor ("Color", COLOR) = (1, 1, 1, 1)
-    _Speed ("Speed", Float) = 1
-    _Period ("Period", Float) = 1
-    _WaveSize ("Wave Size", Float) = 1
-    _Gap ("Gap", Float) = 1
-    _WrinkleFrequency ("Wrinkle Frequency", Float) = 1
-    _WrinkleSize ("Wrinkle Size", Float) = 1
 }
-
 SubShader {
     Tags { "RenderType"="Opaque" }
-    LOD 80
-    
-    Pass
-    {
-        Tags{ "RenderType" = "Opaque" }
+    LOD 150
 
-        CGPROGRAM
+CGPROGRAM
+#pragma surface surf Lambert noforwardadd
 
-        #pragma vertex vert
-        #pragma fragment frag
-        #pragma target 2.0
-        #include "UnityCG.cginc"
-        #include "noiseSimplex.cginc"
+fixed4 _MainColor;
 
-        fixed4 _MainColor;
-        float _Speed;
-        float _Period;
-        float _WaveSize;
-        float _Gap;
-        float _WrinkleFrequency;
-        float _WrinkleSize;
+struct Input {
+    float2 uv_MainTex;
+};
 
-        struct appdata
-        {
-            float3 pos : POSITION;
-            UNITY_VERTEX_INPUT_INSTANCE_ID
-        };
-
-        struct v2f
-        {
-            float4 pos : SV_POSITION;
-
-            UNITY_VERTEX_OUTPUT_STEREO
-        };
-
-        v2f vert(appdata IN)
-        {
-            v2f o;
-            UNITY_SETUP_INSTANCE_ID(IN);
-            UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(o);
-            
-            float3 pos;
-            
-            pos.x = IN.pos.x;
-            pos.y = (sin(IN.pos.z * _Period + _Time.y * _Speed) + sin(IN.pos.z * _Period * 0.4f + _Time.y * _Speed) + snoise(float2(IN.pos.x + _Time.y, IN.pos.z + _Time.y))) * _WaveSize;
-            pos.z = IN.pos.z + sin((IN.pos.x + _Time.y * _Gap) * _WrinkleFrequency) * _WrinkleSize + sin((IN.pos.z * sin(_Time.y) + IN.pos.x * cos(_Time.y)) * _Period * 0.4f + _Time.y * _Speed);
-
-            o.pos = UnityObjectToClipPos(pos);
-            
-            return o;
-        }
-
-        fixed4 frag(v2f IN) : SV_Target
-        {
-            fixed4 col;
-            
-            col.rgb = _MainColor.rgb;
-            col.a = 1.0f;
-
-            return col;
-        }
-
-        ENDCG
-    }
+void surf (Input IN, inout SurfaceOutput o) {
+    fixed4 c = _MainColor;
+    o.Albedo = c.rgb;
+    o.Alpha = c.a;
 }
+ENDCG
+}
+
+Fallback "Mobile/VertexLit"
 }
