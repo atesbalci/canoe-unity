e1a83ad951b649f782ea10cc21fa403ed567cbe9
sercan gedik <sercangedik@gmail.com>
2019-07-12 18:28:19 +0300

    lobby first commit

---
+++ Assets/UIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> playerAvatarList;
+    [SerializeField] private GameObject text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void PlayerCame(int playerNo)
+    {
+        
+    }
+}
