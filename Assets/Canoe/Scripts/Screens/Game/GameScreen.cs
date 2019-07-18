39664fbee35b8b3adb3aefb4b008477bab0b0f22
Ates Balci <abalci@zynga.com>
2019-07-18 17:19:13 +0300

    Merged network and gameplay

--- Assets/Canoe/Scripts/Screens/Game/GameScreen.cs
+++ Assets/Canoe/Scripts/Screens/Game/GameScreen.cs
@@ -1,28 +1,52 @@
 using System;
+using System.Collections.Generic;
+using Canoe.Game.Models;
+using Canoe.Managers.Game;
 using Canoe.Messages;
+using Canoe.Models;
 using Canoe.Screens.Game.Systems.MessageFactory;
 using Framework.Scripts;
 using Framework.Scripts.Managers.WebSocketServer;
 using UnityEngine;
+using Zenject;
 
 namespace Canoe.Screens.Game
 {
     public class GameScreen : BaseScreen<GameScreenResources>
     {
         private WebSocketServerManager _wssManager;
+        private GameManager _gameManager;
 
         private MessageFactorySystem _messageFactorySystem;
+        private Boat _boat;
+        private IDictionary<UserModel, Player> _playersByUser;
+
+        [Inject]
+        public void Initialize(Boat boat)
+        {
+            _boat = boat;
+        }
 
         protected override void Awake()
         {
             base.Awake();
 
             _wssManager = FindObjectOfType<WebSocketServerManager>();
+            _gameManager = FindObjectOfType<GameManager>();
             _wssManager.OnMessageReceive += OnMessageReceive;
 
             _messageFactorySystem = FindObjectOfType<MessageFactorySystem>();
 
             _messageFactorySystem.OnSwipeMessage += OnSwipeMessage;
+
+            _playersByUser = new Dictionary<UserModel, Player>();
+            foreach (var user in _gameManager.Users)
+            {
+                if (user != null)
+                {
+                    _playersByUser.Add(user, _boat.AddPlayer(user.AvatarId));
+                }
+            }
         }
 
         private void OnDestroy()
@@ -43,8 +67,9 @@ namespace Canoe.Screens.Game
             
             if (message.direction.Equals("up")) direction = Vector2.up;
             else if (message.direction.Equals("down")) direction = Vector2.down;
-            
-            Debug.Log(direction);
+
+            var user = _gameManager.FindUserByClientSocket(clientSocket);
+            _playersByUser[user].Row(Time.time, direction == Vector2.down);
         }
     }
 }
