0a9344787035b037f2f5be7b88bd4572a52abc84
sercan gedik <sercangedik@gmail.com>
2019-07-18 10:12:39 +0300

    level design and camera movement

---
+++ Assets/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> prefabList; 
+    private List<GameObject> roadList = new List<GameObject>(); 
+    private const int ROAD_LENGTH = 164;
+    private int oldCameraPositionIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Prefablist : " + prefabList.Count);
+        for (int i = 0; i < prefabList.Count; i++)
+        {
+            roadList.Add(Instantiate(prefabList[i], new Vector3(0, 0, ROAD_LENGTH * i), Quaternion.identity));
+        }    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var newCameraPositionIndex = (int) Camera.main.transform.position.z / ROAD_LENGTH;
+        if (newCameraPositionIndex != oldCameraPositionIndex)
+        {
+            roadList[oldCameraPositionIndex % roadList.Count].transform.position = new Vector3(0, 0, roadList[(oldCameraPositionIndex + roadList.Count - 1) % roadList.Count].transform.position.z + ROAD_LENGTH);
+        }
+
+        oldCameraPositionIndex = (int) Camera.main.transform.position.z / ROAD_LENGTH;
+    }
+}
---
+++ Assets/camera.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    private float counter = 0;
+    private float multiplier = 1;
+    private float direction = 0;
+    private Vector3 movedPosition = new Vector3();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = Mathf.Sin (counter) * multiplier;
+        counter += Time.deltaTime;
+        var transformRotation = Camera.main.transform.rotation.eulerAngles;
+        Debug.Log("transformRotation : " + transformRotation);
+        Camera.main.transform.eulerAngles = new Vector3(transformRotation.x, transformRotation.y, direction);
+        
+        var transformPosition = Camera.main.transform.position;
+        movedPosition.x = transformPosition.x;
+        movedPosition.y = transformPosition.y;
+        movedPosition.z = transformPosition.z + 0.05f;
+        
+        Camera.main.transform.position = movedPosition;
+    }
+}
